01801dce4fa42bb6ea3136c41a04897168a2dcd7
Claude Abounegm <claude.abounegm@gmail.com>
2019-11-19 20:32:46 -0500

    Initial release boilerplate

---
+++ test/index.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+/* global describe it beforeEach afterEach  */
+
+'use strict';
+
+const { expect } = require('chai');
+
+describe('module', function() {
+    it('example test', function() {
+        expect(true);
+    });
+});
